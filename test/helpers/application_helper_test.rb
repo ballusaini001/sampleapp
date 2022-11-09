c22e6abb19f903f543ca2c7f8b637fb512252559
Balchand saini <ballu.saini2705@gmail.com>
2022-11-09 13:00:17 +0530

    Finish layout and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+# require "test_helper"
+
+# class ApplicationHelperTest < ActionView::TestCase
+#   test "full title helper" do
+#     assert_equal root_path, full_title ('Home')
+#     assert_equal help_path, full_title("Help)
+#   end
+# end
